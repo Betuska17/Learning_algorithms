d4ddf40e63a385ce6a860f5436fcd5b0d6ee8f46
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-09-03 19:11:02 +0000

    agregrando las comidas y bebidas

--- multiples_argumentos.py
+++ multiples_argumentos.py
@@ -44,3 +44,49 @@ def assign_food_items(table_number, **order_items):
 assign_table(2, 'Alberto' , True)
 assign_food_items(2, food='Pancakes, Poached Egg', drinks='Water')
 print(tables)
+
+
+############## Complementando y agregando cosas para que ahora muestre tambien las ordenes divididas en
+# comidas y bebidas
+
+
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+
+def assign_table(table_number, name, vip_status = False ):
+    tables[table_number]['name'] = name
+    tables[table_number]['vip_status'] = vip_status
+    tables[table_number]['orders'] = {}
+
+assign_table(2, 'Douglas')
+print(f'-------Tables with Doubglas---------- \n {tables}')
+
+
+# agrego la funcion para que me de las comidas y bebidas por separado
+
+def assign_food_items(table_number, **order_items):
+    tables[table_number]['orders']['food'] = order_items.get('food')
+    tables[table_number]['orders']['drinks'] = order_items.get('drinks')
+
+print('\n --- tables after update --- \n')
+
+assign_food_items(2, food= 'Seabass, Gnocchi, Pizza', drinks= 'Margarita, Water')
+
+
+print(tables)
+  
