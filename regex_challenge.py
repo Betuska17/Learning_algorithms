abf4d3a2e6405b48451f8f5ad700eb891c004eae
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-09-06 07:33:41 -0400

    Update regex_challenge.py

--- regex_challenge.py
+++ regex_challenge.py
@@ -101,9 +101,23 @@ else:
 
 
 
-AC30031160
-AC30026421
-AC30005145
-AC30036460
-AC30126233
+class Employee():
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+
+# Write your code below
+class Admin(Employee):
+  pass
+
+
+e1 = Employee()
+e2 = Employee()
+e3 = Admin()
+e3.say_id()
+
 
