a10cd2ca5d80d8e7bc903bfea4c800069ed9fba7
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-09-06 09:49:58 -0400

    Update regex_challenge.py

--- regex_challenge.py
+++ regex_challenge.py
@@ -1,106 +1,3 @@
-#!/usr/bin/python3
-import os
-import sys
-
-totes = input('Write tote numbers, once you finish enter done\n')
-
-replaced = totes.replace(',', ' ')
-lst = replaced.split()
-send = input('Want to send this totes?\n')
-if send.lower() in ['yes' , 'y']:
-    target = input("INSERT TARGET: \n")
-    osrid = os.environ['OSR_ID']
-    #osrid = osrid[-1]
-
-    #osr = int(input('Osr number: \n'))
-    for tote in lst:
-        command = f"get_tray --tray {tote} --target {target} --osr-id {osrid}"
-        run = os.system(command)
-        print(f'Running: {command}')
-else:
-    for tote in lst:
-        print("'" + tote + "',")
-        
-        
-
-######
-#!/usr/bin/python3
-import os
-import re
-
-# Mensaje recibido (puede ser copiado desde WhatsApp, correo, etc.)
-mensaje = input("Paste the message containing tote numbers:\n")
-
-# Buscar códigos tipo AC3000126354
-totes = re.findall(r"AC\d{10,}", mensaje)
-
-# Si no se encontró ningún código, pedirlos manualmente
-if not totes:
-    manual = input('No totes found. Write tote numbers separated by commas:\n')
-    replaced = manual.replace(',', ' ')
-    totes = replaced.split()
-
-# Confirmar envío
-send = input('Want to send these totes?\n')
-if send.lower() in ['yes', 'y']:
-    target = input("INSERT TARGET:\n")
-    osrid = os.environ.get('OSR_ID', 'default_id')  # Fallback si no está definida
-
-    for tote in totes:
-        command = f"get_tray --tray {tote} --target {target} --osr-id {osrid}"
-        print(f'Running: {command}')
-        os.system(command)
-else:
-    print("Totes parsed:")
-    for tote in totes:
-        print(f"'{tote}',")
-
-
-########################################################################################################
-
-
-
-#!/usr/bin/python3
-import os
-import sys
-import re
-from my_colors import MY_COLORS as COLORS
-
-print(f"{COLORS.GREEN}-------------------------------------------------------------------------------{COLORS.END}")
-print("Write tote numbers (one per line). Press Enter on an empty line to finish:")
-lines = list(iter(lambda: input().strip(), ''))
-totes = ' '.join(lines)
-print(f"{COLORS.GREEN}-------------------------------------------------------------------------------{COLORS.END}")
-
-totes_detected = re.findall(r"\bAC\d{8,}", totes)
-
-if totes_detected:
-    lst = totes_detected
-else:
-    replaced = totes.replace(',',' ')
-    lst = replaced.split()
-print(f"{COLORS.RED}-------------------------------------------------------------------------------{COLORS.END}")
-send = input('Want to send this totes?\n')
-print(f"{COLORS.RED}-------------------------------------------------------------------------------{COLORS.END}")
-
-if send.lower() in ['yes' , 'y']:
-    print(f"{COLORS.PURPLE}-------------------------------------------------------------------------------{COLORS.END}")
-    target = input("Insert Target: \n")
-    print(f"{COLORS.PURPLE}-------------------------------------------------------------------------------{COLORS.END}")
-    osrid = os.environ['OSR_ID']
-    #osrid = osrid[-1]
-
-    #osr = int(input('Osr number: \n'))
-    for tote in lst:
-        command = f"get_tray --tray {tote} --target {target} --osr-id {osrid}"
-        run = os.system(command)
-        print(f'Running: {command}')
-else:
-    for tote in lst:
-        print("'" + tote + "',")
-
-
-
 class Employee():
   new_id = 1
   def __init__(self):
@@ -110,14 +7,16 @@ class Employee():
   def say_id(self):
     print("My id is {}.".format(self.id))
 
-# Write your code below
 class Admin(Employee):
-  pass
+  def say_id(self):
+    super().say_id()
+    print("I am an admin.")
+
+# Write your code below
+class Manager(Admin):
+  def say_id(self):
+    super().say_id()
+    print("Im in charge")
 
 
-e1 = Employee()
-e2 = Employee()
-e3 = Admin()
-e3.say_id()
-
-
+e4 = Manager()
