f1153b255b51cfdb02561913ecff008ed0df5ffc
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-09-12 20:33:54 +0000

    Puse cosas dentro del archivo favorite_places.py

---
+++ python_crash_course/diccionaries/dics_inside_dics/favorite_places.py
@@ -0,0 +1,9 @@
+favorite_places = {
+    'Alberto' : 'Noruega',
+    'Laura': 'Egipto',
+    'Victoria': 'Venecia'
+
+}
+
+for name, place in favorite_places.items():
+    print(f"{name.title()}'s favorite place is {place.title()}")
