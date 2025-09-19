c1b42d088d436588163b0cab4077a1ea421a2c70
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-09-19 20:02:55 +0000

    trying to solve the issues

--- python_crash_course/files_and_exceptions/file_reader.py
+++ python_crash_course/files_and_exceptions/file_reader.py
@@ -3,3 +3,12 @@ with open('pi_digits.txt') as file_object:
     #guardo en contents el resultado de leer file_object
     contents = file_object.read()
     print(contents)
+
+
+
+
+>>> /home/codespace/.python/current/bin/python /workspaces/Learning_algorithms/python_crash_course/files_and_exceptions/file_reader.py
+  File "<stdin>", line 1
+    /home/codespace/.python/current/bin/python /workspaces/Learning_algorithms/python_crash_course/files_and_exceptions/file_reader.py
+    ^
+SyntaxError: invalid syntax
