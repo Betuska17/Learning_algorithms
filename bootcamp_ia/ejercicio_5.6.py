8fbc39da7d1a2d0b6546bed49bc7c48a06d52641
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-10-18 14:23:11 +0000

    changes

--- bootcamp_ia/ejercicio_5.6.py
+++ bootcamp_ia/ejercicio_5.6.py
@@ -23,41 +23,13 @@ for palabra in frecuencia:
 print("Cantidad de hápax en el documento:", 
 
 
-WITH ultima_fecha AS (
-  SELECT tsth_transporter_id, MAX(tsth_date) AS fecha_última
-  FROM transporter_state_trans_hist
-  GROUP BY tsth_transporter_id
-),
-estado_reciente AS (
-  SELECT tsth_transporter_id, transporter_state, tsth_date
-  FROM transporter_state_trans_hist
-  JOIN ultima_fecha 
-    ON tsth_transporter_id = ultima_fecha.tsth_transporter_id
-   AND tsth_date = ultima_fecha.fecha_última
-)
-SELECT 
-  transporter_name,
-  transporter_pos,
-  estado_reciente.tsth_date,
-  CASE 
-    WHEN estado_reciente.transporter_state = -5 THEN 'suspended'
-    WHEN estado_reciente.transporter_state = 10 THEN 'maintence'
-    WHEN estado_reciente.transporter_state = -10 THEN 'disabled'
-    ELSE 'unknown'
-  END AS state
-FROM transporter_locations
-JOIN locations ON tl_loc_id = loc_id
-JOIN transporters ON tl_transporter_id = transporter_id
-JOIN estado_reciente ON tl_transporter_id = estado_reciente.tsth_transporter_id
-WHERE transporter_name LIKE 'SHUT%'
-  AND transporter_osr_id = 1 
-  AND transporter_pos LIKE '%C0%'
-  AND estado_reciente.transporter_state != 0;
+
   
   
   
   
   
+<<<<<<< Updated upstream
 WITH ultima_fecha AS (
   SELECT tsth_transporter_id, MAX(tsth_date) AS fecha_última
   FROM transporter_state_trans_hist
@@ -124,6 +96,8 @@ WHERE tr.transporter_name LIKE 'SHUT%'
   AND tr.transporter_osr_id = 1 
   AND tl.transporter_pos LIKE '%C0%'
   AND ls.transporter_state != 0;
+=======
+>>>>>>> Stashed changes
 
 
 
