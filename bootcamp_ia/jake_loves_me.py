9fce377a5e144eae592bb17389d8627ecad18ec4
Betuska17 <141220044+Betuska17@users.noreply.github.com>
2025-10-18 16:21:28 -0400

    Actualizar jake_loves_me.py

--- bootcamp_ia/jake_loves_me.py
+++ bootcamp_ia/jake_loves_me.py
@@ -1,36 +1,32 @@
 from my_colors import MY_COLORS as colors
 from datetime import datetime
+
 now = datetime.now()
 
 for value in output:
-        shuttle = value[0]
-        location = value[1]
-        state = value[3]
-        time = value[2]
+    shuttle = value[0]
+    location = value[1]
+    state = value[3]
+    time = value[2]
 
-        minutes = (now - time).total_seconds() / 60
-        if minutes < 20:
-            color = colors.GREEN
-        elif minutes < 60:
-            color = colors.YELLOW
-        else:
-            color = colors.RED
+    minutes = (now - time).total_seconds() / 60
 
-        if minutes < 60:
-            time_str = f"{int(minutes)} seconds"
-            #print(f"{color}{shuttle} {state} at {location} since --> {time} ({time_str}) ago {reset}")
-            #reset = "\033[0m"
+    # Semáforo por tiempo
+    if minutes < 20:
+        color = colors.GREEN
+    elif minutes < 60:
+        color = colors.YELLOW
+    else:
+        color = colors.RED
 
-        elif minutes < 3600:
-            time_str = f"{int(minutes // 60)} minutes"
-            #print(f"{color}{shuttle} {state} at {location} since --> {time} ({time_str}) ago {reset}")
-            #reset = "\033[0m"
+    # Formato de tiempo
+    if minutes < 1:
+        time_str = f"{int(minutes * 60)} segundos"
+    elif minutes < 60:
+        time_str = f"{int(minutes)} minutos"
+    else:
+        horas = minutes / 60
+        time_str = f"{horas:.1f} horas"
 
-        else:
-            horas = minutes / 3600
-            time_str = f"{horas:.1f} hours"
-           # print(f"{color}{shuttle} {state} at {location} since --> {time} ({time_str}) ago {reset}")
-           # reset = "\033[0m"
-
-            reset = colors.WHITE
-            print(f"{color}{shuttle} {state} at {location} since --> {time} ({time_str}) ago{reset}")
+    reset = colors.WHITE
+    print(f"{color}{shuttle} {state} at {location} since --> {time} ({time_str} ago){reset}")
